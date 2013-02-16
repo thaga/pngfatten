dfcf70dfcc5e1177c465dbf273e20812e3a21d51
Haga Takeshi <haga.takeshi@dena.jp>
2013-02-16 23:16:49 +0900

    comment

--- pngfatten.jsx
+++ pngfatten.jsx
@@ -3,7 +3,7 @@ import 'console.jsx';
 
 class _Main {
 	static function main(args:string[]):void {
-		var rcv = dom.id('rcv') as HTMLDivElement;
+		var rcv = dom.id('rcv') as HTMLDivElement; // DnD receiver
 
 		rcv.ondragover = function(e:Event):void {
 			e.preventDefault();
@@ -11,14 +11,20 @@ class _Main {
 		rcv.ondrop = function(e:Event):void {
 			e.preventDefault();
 
-			var de = e as __noconvert__ DragEvent;
+			var de = e as __noconvert__ DragEvent; // CAUTION: Chrome creates MouseEvent
 			var file = de.dataTransfer.files[0];
 
 			var file_reader = new FileReader;
 			file_reader.onload = function(e:Event):void {
+				// now file reading complete
 				var img = dom.document.createElement('img') as HTMLImageElement;
 				img.onload = function(e:Event):void {
+					// now image creation complete
+
+					// process image
 					var img_fat = _Main.fatten(img);
+
+					// display result
 					dom.document.body.appendChild(img);
 					dom.document.body.appendChild(img_fat);
 				};
@@ -54,8 +60,10 @@ class _Main {
 	}
 
 	static function fatten(px:Uint8ClampedArray, w:int, h:int):void {
+		// clone pixel array
 		var px2 = new Uint8ClampedArray(px);
 
+		// process loop
 		for (var y = 0; y < h; ++y) for (var x = 0; x < w; ++x) {
 			var bi = (y * w + x) * 4; // base index
 
@@ -63,6 +71,7 @@ class _Main {
 
 			var r = 0, g = 0, b = 0, a = 0;
 
+			// sampling loop
 			for (var dy = -1; dy <= 1; ++dy) {
 				var sy = y + dy; // sample y
 				if (sy < 0 || sy >= h) continue;
@@ -73,10 +82,10 @@ class _Main {
 
 					var sbi = (sy * w + sx) * 4; // sample base index
 
-					var sa = px2[sbi + 3];
-					r += px2[sbi] * sa;
-					g += px2[sbi+1] * sa;
-					b += px2[sbi+2] * sa;
+					var sa = px2[sbi + 3]; // sampled alpha
+					r += px2[sbi] * sa;   // alpha weighted color
+					g += px2[sbi+1] * sa; // alpha weighted color
+					b += px2[sbi+2] * sa; // alpha weighted color
 					a += sa;
 				}
 			}
